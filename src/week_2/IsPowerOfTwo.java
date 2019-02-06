121d8fbe7980bb13685b125b311f4e7314e11555
Yulia Volkova <yuulia.volkova@gmail.com>
2019-02-06 23:45:26 +0000

    add is IsPowerOfTwo programme Integer class method version

--- src/week_2/IsPowerOfTwo.java
+++ src/week_2/IsPowerOfTwo.java
@@ -5,18 +5,33 @@ package week_2;
  */
 public class IsPowerOfTwo {
     public static void main(String[] args) {
-        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwoIntegerClass(32));
+    }
+    static void convertToBinary(int no){
+        int container[] = new int[100];
+        int i = 0;
+        while (no > 0){
+            container[i] = no%2;
+            i++;
+            no = no/2;
+        }
+        for (int j = i -1 ; j >= 0 ; j--){
+        }
     }
 
     public static boolean isPowerOfTwo(int value) {
         if (value == 0) {
             return false;
         }
-        if (value == 1) {
-            return true;
-        }
         int absValue =  Math.abs(value);
-        String binaryRepresentation = Integer.toBinaryString(absValue-1);
-        return !binaryRepresentation.contains("0");
+        int val2 = absValue-1;
+        convertToBinary(absValue);
+        convertToBinary(val2);
+        return ((absValue & val2 )== 00000000);
+    }
+
+    public static boolean isPowerOfTwoIntegerClass(int value) {
+        int absValue =  Math.abs(value);
+        return Integer.bitCount(absValue) == 1;
     }
 }
