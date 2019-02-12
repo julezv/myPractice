325beaf0de4c1062754d2625a48ed088ec5db412
Yulia Volkova <yulia.volkova@behavox.com>
2019-02-12 18:02:15 +0000

    added task4 from Homework3

---
+++ src/week_3/MaxFinder.java
@@ -0,0 +1,20 @@
+package week_3;
+
+/**
+ * Created by ulya on 12.02.19.
+ */
+public class MaxFinder {
+    public static void main(String[] args) {
+        System.out.println(findMax(new int []{-3,-5,-4}));
+    }
+    public static int findMax(int [] a) {
+        int i;
+        int max = a[0];
+        for(i = 0; i < a.length; ++i){
+            if (a[i]> max){
+                max = a[i];
+            }
+        }
+        return max;
+    }
+}
