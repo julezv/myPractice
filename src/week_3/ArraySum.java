d1eaa49c7d9bb6c6ff277264b4da1455dd740b39
Yulia Volkova <yuulia.volkova@gmail.com>
2019-02-17 11:28:39 +0000

    update with comments from class - String vs StringBuilder - make one argument always larger than the other to avoid comparing - Arrays.toString() method # Conflicts: #	.idea/vcs.xml #	src/week_2/Point.java

--- src/week_3/ArraySum.java
+++ src/week_3/ArraySum.java
@@ -1,48 +1,63 @@
 package week_3;
 
+import java.util.Arrays;
+
 /**
  * Написать программу, которая вычисляет поэлементную сумму двух массивов.
  * То есть если заданы два массива, возможно разной длины, то программа формирует
  * по ним третий массив, элементами которого являются поэлементные суммы для тех индексов,
  * для которых есть элементы в обоих заданных массивах, и элементы более длинного массива для остальных индексов.
  */
-public class ArraySum {
+public class ArraySum{
     public static void main(String[] args) {
-        System.out.println(convertToJoinedString(sum(new int []{-3,-5,-4, 6, 5, 3}, new int []{-3, 40})));
+        int[] a = new int[] {1, 2, 3, 4, 5, 6};
+        String s1 = "";
+        for (int i = 0; i < a.length; i++) {
+            s1 += a[i]; //1 => "1" => s1 =  "" + "1" = "1"
+            //2 => "2" => s1 = "1" + "2" = "12"
+            //3 => "3" => s1 = "12" + "3" = "123"
+            // ... s2 = "123456"
+        }
+        System.out.println(s1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < a.length; i++) {
+            sb.append(a[i]);
+        }
+        System.out.println(sb); //System.out.println(sb.toString());
+
+        System.out.println(a); // System.out.println(a.toString())
+
+        System.out.println(Arrays.toString(a));
+
+        System.out.println(convertToJoinedString(a));
+
+
+        System.out.println(convertToJoinedString(sum(new int []{-3, 40}, new int []{-3,-5,-4, 6, 5, 3})));
     }
-    public static int[] sum(int [] a, int [] b) {
-        int i;
-        int j;
-        int container[] = new int[a.length];
-        if (a.length >= b.length) {
-            container = new int[a.length];
-            for (i = 0; i < b.length; ++i) {
-                container[i] =  a[i] + b[i];
-            }
-            for(j = i; j < a.length; j++ ){
-                container[j] = a[j];
-            }
+    public static int[] sum(int[] a, int[] b) {
+        if (a.length < b.length) {
+            int[] c = a;
+            a = b;
+            b = c;
+        } // a.length >= b.length
+
+        int[] sum = new int[a.length];
+        for (int i = 0; i < b.length; ++i) {
+            sum[i] =  a[i] + b[i];
         }
-        if (b.length > a.length) {
-            container = new int[b.length];
-            for (i = 0; i < a.length; ++i) {
-                container[i] =  a[i] + b[i];
-            }
-            for(j = i; j < b.length; j++ ){
-                container[j] = b[j];
-            }
+        for (int i = b.length; i < a.length; i++){
+            sum[i] = a[i];
         }
-        return container;
+
+        return sum;
     }
 
     private static String convertToJoinedString(int[] numbers) {
         StringBuilder builder = new StringBuilder();
-        int[] var2 = numbers;
-        int var3 = numbers.length;
-        for (int var4 = 0; var4 < var3; ++var4) {
-            int number = var2[var4];
-            builder.append(number + " ");
+        for (int i = 0; i < numbers.length; ++i) {
+            builder.append(numbers[i] + " ");
         }
-        return builder.toString();
+        return builder.toString(); //{1, 2, 3, 4, 5} => "1 2 3 4 5"
     }
 }
