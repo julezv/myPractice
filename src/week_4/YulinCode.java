7e505fa0374b1ab5f89020b967791133f8d87471
Yulia Volkova <yuulia.volkova@gmail.com>
2019-02-27 08:20:36 +0000

    update code, change from HashMap to int[] for location of even and odd numbers

--- src/week_4/YulinCode.java
+++ src/week_4/YulinCode.java
@@ -1,13 +1,17 @@
 package week_4;
 
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.stream.Collectors;
 
 /**
  * Created by yuliav on 20/02/2019.
  */
 
 public class YulinCode {
+    public static void main(String[] args) {
+        System.out.println((yuliaComplementRepresentation(0)));
+    }
+
     private static void reverse(int[] data) {
         int left = 0;
         for (int right = data.length - 1; left < right; --right) {
@@ -20,7 +24,10 @@ public class YulinCode {
 
     private static int[] convertToBinary(int a) {
         int[] ar = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-        if (a > 256) {
+        if (a == 0) {
+            return ar;
+        }
+        if (a > 255) {
             throw new ArithmeticException("Absolute value of positive input cannot be larger than 256");
         } else {
             while (a >= 2) {
@@ -34,42 +41,41 @@ public class YulinCode {
         }
     }
 
-    private static int[] dealWithPositiveNumbersYulinCode(int a) {
-        if (a > 128) {
-            throw new ArithmeticException("Absolute value of positive input cannot be larger than 127");
-        } else {
-            HashMap<Integer, Integer> positiveMap = new HashMap<>();
-            int j = 2;
-            for (int i = 1; i <= 128; ++i) {
-                positiveMap.put(i, j);
-                j += 2;
-            }
-            return (convertToBinary(positiveMap.get(a)));
-        }
-    }
-
-    public static int[] dealWithNegativeNumbersYulinCode(int a) {
-        if (Math.abs(a) > 128) {
-            throw new ArithmeticException("Absolute value of negative input cannot be larger than 128");
-        } else {
-            HashMap<Integer, Integer> negativeMap = new HashMap<>();
-            int j = 1;
-            for (int i = -1; i >= -128; --i) {
-                negativeMap.put(i, j);
-                j += 2;
-            }
-            return (convertToBinary(negativeMap.get(a)));
-        }
-    }
 
     public static String yuliaComplementRepresentation(int a) {
-        int[] ar = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-        if (a > 0) {
-            return Arrays.toString(dealWithPositiveNumbersYulinCode(a));
+        if (a > 0 && a > 127) {
+            throw new ArithmeticException("Absolute value of positive input cannot be larger than 127");
         }
-        if (a < 0) {
-            return Arrays.toString(dealWithNegativeNumbersYulinCode(a));
+        if (a < 0 && Math.abs(a) > 128) {
+            throw new ArithmeticException("Absolute value of negative input cannot be larger than 128");
         }
-        return Arrays.toString(ar);
+        int[] arr = new int[256];
+        int j = -1;
+        int k = 1;
+        for (int i = 0; i <= 255; ++i) {
+            if (i == 0) {
+                arr[i] = 0;
+                continue;
+            }
+            if (i % 2 == 0) {
+                arr[i] = k;
+                k++;
+            }
+            if (i % 2 != 0) {
+                arr[i] = j;
+                j--;
+            }
+        }
+        System.out.println(findIndex(arr, a));
+        return Arrays.toString(convertToBinary(findIndex(arr, a)));
     }
+
+
+    public static int findIndex(int[] a, int target) {
+        return Arrays.stream(a)
+                .boxed()
+                .collect(Collectors.toList())
+                .indexOf(target);
+    }
+
 }
